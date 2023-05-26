c758f8f112ab6ed1bd4cc08bc4fd95fe4a26fcd6
shradha <shradhajain8@deloitte.com>
2023-05-26 09:25:38 +0530

    initial commit

---
+++ miniProject/src/test/java/com/springBoot/miniProject/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.miniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
